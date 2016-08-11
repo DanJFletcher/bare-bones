db3c40cb0c24f96106f02ba7f369f8b020c17c62
Dan Fletcher <dannyjfletcher@outlook.com>
2016-08-11 02:15:24 +0000

    Plug templates into index.php

--- src/index.php
+++ src/index.php
@@ -1,56 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    	<title>Beard Pack</title>
+<?php
+/**
+ * Page: Home
+ * 
+ */
+ 
+ // Requires/Includes go here.
+ require('env.php');
+ 
+ // Generate the header markup
+ include('header.php');
+ 
+?>
 
-        <!--META-->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- TODO: Add page content -->
 
-        <!--CSS-->
-        <link href="css/stylesheet.css" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-        
-        <link rel="stylesheet" href="./vendor/css/bower.css" type="text/css" />
+<?php 
+
+// Gernerate the footer markup
+include('footer.php') 
+
+?>
 
 
-    </head>
 
-    <!--BODY-->
-    <body>
-
-    	<!--HEADER-->
-        <header class="full">
-            <div id="header-menuholder" class="full">
-            	<div class="center">
-                	<div id="header-logo">
-                    	<a href="index.html" title="Home"><img src="images/logos/beard.png" alt="Beard"></a>
-                  </div>
-
-                  <div id="header-menu">
-                  	<div class="header-menuitem">
-                      	home
-                      </div>
-                  	<div class="header-menuitem">
-                      	about
-                      </div>
-                  	<div class="header-menuitem">
-                      	products
-                      </div>
-                  	<div class="header-menuitem">
-                      	gallery
-                      </div>
-                  	<div class="header-menuitem">
-                      	contact
-                      </div>
-                      <div class="clear"></div>
-                  </div> <!-- end #header-menu -->
-                  <div class="clear"></div>
-              </div>
-            </div>
-        </header>
-        
-        <script>console.log(jQuery)</script>
-    </body>
-</html>
