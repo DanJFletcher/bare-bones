1b866fb5f91dc2a1dd188eb0a896cb55007b614f
Dan Fletcher <dannyjfletcher@outlook.com>
2016-08-11 02:29:53 +0000

    Edit vendor CSS links: header.php

--- src/header.php
+++ src/header.php
@@ -1,3 +1,13 @@
+<?php 
+/**
+ * Template: Header
+ * 
+ */
+ 
+ // Requires go here.
+ require('../env.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,13 +16,17 @@
         <!--META-->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Reset CSS -->
+        <link rel="stylesheet" href="<?php echo SITE_URL ?>/vendor/normalize-css/normalize.css" type="text/css" />
+        
+        <!-- CSS Libs/Frameworks -->
+        <link rel="stylesheet" href="<?php echo SITE_URL ?>/vendor/bootstrap/bootstrap.min.css" type="text/css" />
 
         <!--CSS-->
         <link href="css/stylesheet.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-        
-        <link rel="stylesheet" href="./vendor/css/bower.css" type="text/css" />
 
 
     </head>
